51f7af400f7e9c5fe044c22c44bc73f7ec7299ea
Jefferson Osowsky <116462366+osowsky@users.noreply.github.com>
2023-08-28 18:14:44 -0300

    Aula 109: remove doubt. global scope/variable.

---
+++ aula-109.py
@@ -0,0 +1,22 @@
+# Scope and global variables.
+# global variable in a internal function only assesses and modifies
+# external variables declared in the scope of the current module. 
+
+x = 1
+
+def fn_1st_level():
+    x = 2
+
+    def fn_2nd_level():
+        global x    # This 'x' refers to 'x' of the module ( x = 1 ). 
+        print( 'fn_2nd_level:', x )
+        x = 3
+        print( 'fn_2nd_level:', x )
+
+    print( 'fn_1st_level:', x )
+    fn_2nd_level()
+    print( 'fn_1st_level:', x )
+
+# print( 'module_level:', x )
+fn_1st_level()
+# print( 'module_level:', x )
