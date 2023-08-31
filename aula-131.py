3c468fe09dfccb5d693538332fb30e33d6bebc53
Jefferson Osowsky <116462366+osowsky@users.noreply.github.com>
2023-08-31 20:01:13 -0300

    Aula 131: exercise. use of set. find the first duplicated number.

---
+++ aula-131.py
@@ -0,0 +1,90 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+s1 = set()  # vazio
+s1 = {'Luiz', 1, 2, 3}  # com dados
+
+# Sets são eficientes para remover valores duplicados
+# de iteráveis.
+# - Não aceitam valores mutáveis;
+# - Seus valores serão sempre únicos;
+# - não tem índexes;
+# - não garantem ordem;
+# - são iteráveis (for, in, not in)
+
+# Métodos úteis:
+# add, update, clear, discard
+
+# Operadores úteis:
+# união | união (union) - Une
+# intersecção & (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em ambos
+
+"""
+Exercício
+Crie uma função que encontra o primeiro duplicado considerando o segundo
+número como a duplicação. Retorne a duplicação considerada.
+Requisitos:
+    A ordem do número duplicado é considerada a partir da segunda
+    ocorrência do número, ou seja, o número duplicado em si.
+    Exemplo:
+        [1, 2, 3, ->3<-, 2, 1] -> 1, 2 e 3 são duplicados (retorne 3)
+        [1, 2, 3, 4, 5, 6] -> Retorne -1 (não tem duplicados)
+        [1, 4, 9, 8, ->9<-, 4, 8] (retorne 9)
+    Se não encontrar duplicados na lista, retorne -1
+"""
+
+import os # use function 'system'
+
+lista_de_listas_de_inteiros = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [9, 1, 8, 9, 9, 7, 2, 1, 6, 8],
+    [1, 3, 2, 2, 8, 6, 5, 9, 6, 7],
+    [3, 8, 2, 8, 6, 7, 7, 3, 1, 9],
+    [4, 8, 8, 8, 5, 1, 10, 3, 1, 7],
+    [1, 3, 7, 2, 2, 1, 5, 1, 9, 9],
+    [10, 2, 2, 1, 3, 5, 10, 5, 10, 1],
+    [1, 6, 1, 5, 1, 1, 1, 4, 7, 3],
+    [1, 3, 7, 1, 10, 5, 9, 2, 5, 7],
+    [4, 7, 6, 5, 2, 9, 2, 1, 2, 1],
+    [5, 3, 1, 8, 5, 7, 1, 8, 8, 7],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+]
+
+os.system( 'cls' ) # clear screen.
+
+# return the first number in a list that is
+# duplicated. otherwise, it returns None.
+def find_duplicate_number( num_lst ):
+    set_A = set()
+
+    for num in num_lst: # get each number in list.
+        # is num in the set_A?
+        if ( num in set_A ): # we can use intersection.
+        # if ( len( set( [ num ] ) & set_A ) != 0 ):
+            return num
+        else:
+            set_A.add( num )
+    return None
+
+
+for l in lista_de_listas_de_inteiros:
+    dup_num = find_duplicate_number( l ) # get the first number duplication.
+
+    # print outcome.
+    if ( dup_num is not None ):
+        print( f'In the list: ', l, )
+        print( f'the first number with more than one occurence is ', dup_num )
+    else:
+        print( f'In the list: ', l, )
+        print( f'there is no duplicated number' )
+    input( 'press any key to continue...' )
+    print()
