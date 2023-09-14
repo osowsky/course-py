0b838abe9a6cdb3b256dba8dcba6ef831d14a74e
Jefferson Osowsky <116462366+osowsky@users.noreply.github.com>
2023-09-14 16:47:15 -0300

    Aula 236-239: Dunder methods: __repr__, __new__, and __init__

--- aula-236-239.py
+++ aula-236-239.py
@@ -64,3 +64,29 @@ if __name__ == '__main__':
     print(p3)
     print('P1 é maior que p2?', p1 > p2)
     print('P2 é maior que p1?', p2 > p1)
+
+
+#-----------------------------------------------------------------------
+# __new__ e __init__ em classes Python
+# __new__ é o método responsável por criar e
+# retornar o novo objeto. Por isso, new recebe cls.
+# __new__ ❗️DEVE retornar o novo objeto❗️
+# __init__ é o método responsável por inicializar
+# a instância. Por isso, init recebe self.
+# __init__ ❗️NÃO DEVE retornar nada (None)❗️
+# object é a super classe de uma classe
+class A:
+    def __new__(cls, *args, **kwargs):
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, x):
+        self.x = x
+        print('Sou o init')
+
+    def __repr__(self):
+        return 'A()'
+
+
+a = A(123)
+print(a.x)
